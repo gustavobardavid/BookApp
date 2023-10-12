d982018ed136d787ba2eb2eaa3f6b8957b95e3cf
gustavo <gustavo.cesariosilva15@gmail.com>
2023-10-12 19:29:08 -0300

    Criando projeto de livros

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import  { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState('javascript');
+
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data.items);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar livros:', error);
+      });
+  }, [query]);
+
+  return (
+    <div className="App">
+      <h1>Busca de Livros</h1>
+      <div>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Digite um termo de pesquisa"
+        />
+      </div>
+      <div className="book-list">
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            <img
+              src={book.volumeInfo.imageLinks?.thumbnail}
+              alt={book.volumeInfo.title}
+            />
+            <h2>{book.volumeInfo.title}</h2>
+            <p>{book.volumeInfo.authors?.join(', ')}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
